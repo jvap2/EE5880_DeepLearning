49315371c00f9b5ea3777f129d3f67495ae57e9f
jvap2 <jayvap6181@gmail.com>
2023-01-18 07:20:32 -0700

    developing unit addition algorithm

--- gen_sys_mod.py
+++ gen_sys_mod.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import math
+from math import comb
 
 data=pd.read_csv("Gen_Reliability.csv")
 data_np=data.to_numpy()
@@ -11,3 +13,9 @@ Maintenance=data_np[:,4]
 MTTF=data_np[:,2]
 failure_rate=np.divide(np.ones(shape=np.shape(MTTF)),MTTF)
 repair_rate=np.divide(np.ones(shape=np.shape(MTTR)),MTTR)
+total_units=np.sum(units)
+combos=0
+for i in range(1,total_units+1):
+    combos+=comb(32,i)
+print(combos)
+
---
+++ reliability_functions.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def Unit_Addition_Algorithm(outage,failure_rate,repair_rate):
+    pass
