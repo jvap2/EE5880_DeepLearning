713351e9a092d52cdf2027ac9158c2134a58f9f5
jvap2 <jayvap6181@gmail.com>
2023-01-18 10:40:35 -0700

    More to unit add and a gitignore

--- gen_sys_mod.py
+++ gen_sys_mod.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import math
 from math import comb
+import reliability_functions
+from reliability_functions import Unit_Addition_Algorithm
 
 data=pd.read_csv("Gen_Reliability.csv")
 data_np=data.to_numpy()
@@ -17,5 +19,14 @@ total_units=np.sum(units)
 combos=0
 for i in range(1,total_units+1):
     combos+=comb(32,i)
-print(combos)
+failure_for_size={}
+repair_for_size={}
+units_for_size={}
+for (i,value) in enumerate(size):
+    failure_for_size[value]=failure_rate[i]
+    repair_for_size[value]=repair_rate[i]
+    units_for_size[value]=units[i]
+Unit_Addition_Algorithm(units_for_size,failure_for_size,repair_for_size)
+
+
 
--- reliability_functions.py
+++ reliability_functions.py
@@ -1,4 +1,11 @@
 import numpy as np
 
-def Unit_Addition_Algorithm(outage,failure_rate,repair_rate):
-    pass
+def Unit_Addition_Algorithm(unit,failure_rate,repair_rate):
+    f=np.empty(shape=len(failure_rate))
+    r=np.empty(shape=len(repair_rate))
+    u=np.empty(shape=len(unit))
+    for (i,key) in enumerate(failure_rate.keys()):
+        f[i]=failure_rate[key]/(failure_rate[key]+repair_rate[key])
+        r[i]=1-f[i]
+        u[i]=unit[key]
+    
