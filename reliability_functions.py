244e3bd221192f2ae9843d629146246d60c4ae80
jvap2 <jayvap6181@gmail.com>
2023-01-26 06:26:53 -0700

    Appear to have correct values now

--- reliability_functions.py
+++ reliability_functions.py
@@ -95,22 +95,25 @@ def Load_Model_Algorithm(min,max,load):
     for elements in load_levels:
         P_L[elements]=0
         F_L[elements]=0
+    J=1E6    
     while i<=N_2:
         i_hour_contrib=load[i]
-        J=floor(i_hour_contrib)//Z
-        for j in range(J+1):
+        J_1=floor(i_hour_contrib)//Z+1
+        for j in range(1,J_1):
             P_L[j]+=1
         if i+1<=N_2:
             J_1=floor(load[i+1])//Z
         if J_1>=J:
-            for q in range(J,J_1+1):
+            for q in range(J,J_1):
                 F_L[q]+=1
+        J=J_1
         i+=1
     for i,(prob,freq) in enumerate(zip(P_L.values(),F_L.values())):
         P_L[i]=prob/N_H
         F_L[i]=freq/N_H
-    P_L={key: item for key, item in P_L.items() if key>964 }
-    F_L={key: item for key, item in F_L.items() if key>964 }
+    P_L[0]=0
+    # P_L={key: item for key, item in P_L.items() if key>964 }
+    # F_L={key: item for key, item in F_L.items() if key>964 }
     return P_L,F_L
 
 
