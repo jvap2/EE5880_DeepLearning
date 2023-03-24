dfd38cbbc5e64ef23165003d9739afa36529fb24
jvap2 <jayvap6181@gmail.com>
2023-03-24 04:24:00 -0600

    Ya

--- nn_reliability.py
+++ nn_reliability.py
@@ -2,30 +2,29 @@ import matplotlib.pyplot as plt
 import torch 
 import numpy as np
 import torch.nn as nn
-from sklearn.model_selection import train_test_split
+
 
 
 class Model(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        super().__init__() ##Call constructor from parent class
+        super(Model,self).__init__() ##Call constructor from parent class
         self.layer1=nn.Linear(input_size,hidden_size)##Give the input and output size
         self.layer2=nn.Linear(hidden_size,output_size)
+        self.output=nn.ReLU()
         
     def forward(self,x):
         '''This function will take an input pattern and do the forward pass'''
         x=self.layer1(x) 
         x=nn.LeakyReLU(.1)(x)
         x=self.layer2(x)
+        x=self.output(x)
         return x
     
 def Loss(output,x,Load,A,T_max):
     '''x[:,0]=Pg and x[:,1]=Pd and x[:,2]=Pl'''
     T=torch.matmul(A,x[:,0]+output-x[:,1])
-    loss=torch.sum((-output)**2)
-    # loss=nn.MSELoss()(x[:,0]+output,x[:,1])+torch.sum(output)
-    # +nn.ReLU(nn.MSELoss()(torch.sum(x[:,0]),3405))+\
-    #     nn.ReLu(nn.MSELoss()(Load-torch.sum(x[:,1])))+nn.MSELoss()(nn.ReLU(T-T_max))+\
-    #     nn.MSELoss()(nn.ReLU(output-x[:,1]))
+    loss=torch.sum((-output)**2)+torch.sum((x[:,0]+output-x[:,1])**2)+nn.ReLU()(torch.sum(x[:,0])-3405)+\
+        nn.ReLU()(Load-torch.sum(x[:,1]))+nn.ReLU()(torch.sum(T-T_max))+nn.ReLU()(torch.sum(output-x[:,1]))
     return loss
 
 
@@ -40,7 +39,7 @@ def Train(model,input,Load,A,T_max,optimizer):
             pred=pred.clone()
             pred[j]=model(val)
             loss=Loss(pred,input,Load,A,T_max)
-            loss.backward(inputs= pred,retain_graph=True)
+            loss.backward(retain_graph=True)
             total_loss=total_loss+loss.item()
             optimizer.step()
     return pred
