7a1117afb3b97cda7c7f41ec096fa666aa58b293
jvap2 <jayvap6181@gmail.com>
2023-04-13 15:31:27 -0600

    Further corrections

--- reliability_functions.py
+++ reliability_functions.py
@@ -335,15 +335,24 @@ def Seq_MC_Comp(load,gen,N,maxCap,A,T,T_max,W,Load_Buses,Load_Data,Gen_data):
             C=np.zeros(shape=np.shape(A)[1])
             time=Gen_data.iloc[:,5].min()
             T_idx_bus=Gen_data.index[Gen_data.iloc[:,5]==time].tolist()
+            print(T_idx_bus)
+            print(Gen_data['State Time'])
             down_state_idx=Gen_data.index[Gen_data.iloc[:,4]==0].tolist()
+            print(down_state_idx)
             Power_Down=Gen_data.loc[down_state_idx,'Cap'].sum()
             Gen_data.iloc[:,5]=Gen_data.iloc[:,5]-time
             state=Gen_data["State"].to_numpy()
+            print(state)
             hr+=time
             if hr>8759:
                 hr=8759
             Cap=maxCap-Power_Down
-            if(state.any()==0):
+            check=False
+            for s in state:
+                if s==0:
+                    check=True
+                    break
+            if(check):
                 for t in range(t_n,hr):
                     Temp_Load=np.array(np.copy(Load_Data),dtype=np.float64)
                     C=PSO_rel(A,T,T_max,Gen_data,load[t],Load_Buses,Temp_Load,Curt,W,Power_Down,alpha=0,beta=0)
@@ -362,13 +371,13 @@ def Seq_MC_Comp(load,gen,N,maxCap,A,T,T_max,W,Load_Buses,Load_Data,Gen_data):
                         check_down=0
             t_n=hr
             for value in T_idx_bus:
-                if state[value-1]==0:
+                if state[value]==0:
+                    print(value)
                     Gen_data.loc[value,'State']=1
                     Gen_data.loc[value,'State Time']=np.int_(np.floor(-np.log(np.random.rand(1))/Gen_data.loc[value,'Failure Rate']))
-                    print("Updating Gen Data Cap")
-                    print(Gen_data.loc[value,'State Time'])
                     Gen_data.loc[value,'Cap']=Pg.loc[value,'Cap']
                 else:
+                    print(value)
                     Gen_data.loc[value,'State']=0
                     Gen_data.loc[value,'State Time']=np.int_(np.floor(-np.log(np.random.rand(1))/Gen_data.loc[value,'Repair Rate']))
                     Gen_data.loc[value,'Cap']=0
@@ -495,7 +504,7 @@ def Constraints(C,A,GD,LD,T_max,Pl):
     Curt=np.dot(np.ones(shape=np.shape(C)),C)
     T=np.matmul(A,(GD+C-LD))
     if T.all()<=T_max.all() and C.all()<=LD.all() and ((GD+C).all()==LD.all()) and sum(GD)<3405:
-        return Curt
+        return C
     else: 
         return 10
     
